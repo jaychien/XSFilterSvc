bd06647defd1b46b227965e69b1c0d1b3d8b66ca
derektu <derektu123@gmail.com>
2014-10-11 23:23:09 +0800

    - add field related API - add search stock API - query.html UI

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var WebServer = require('./lib/webserver.js');
 var options = require('./lib/config.json');
 
 options = options || {};
+options.xsserver = options.xsserver || 'http://203.67.19.128/xsservicetest/';
 options.port = options.port || 7001;
 
-new WebServer(options.port).run();
+new WebServer(options).run();
